d1799cbf2af5928be7b1647b70f6aabea0c13791
yoridrums <89488616+yoridrums@users.noreply.github.com>
2023-05-02 14:12:03 +0200

    Create checkrdp.ps1

---
+++ checkrdp.ps1
@@ -0,0 +1,20 @@
+$cred = Get-Credential
+$hosts = Get-Content -Path "C:\hosts.txt"
+
+foreach ($host in $hosts) {
+    $error.Clear()
+    $connection = New-Object -ComObject "MsRdp.RdpClient"
+    $connection.Server = $host
+    $connection.AdvancedSettings2.ClearTextPassword = $cred.GetNetworkCredential().Password
+    $connection.UserName = $cred.GetNetworkCredential().UserName
+
+    $connection.Connect()
+
+    if ($connection.Connected) {
+        Write-Host "You can log in to $host with your current credentials."
+        $connection.Disconnect()
+    }
+    else {
+        Write-Host "Failed to connect to $host. Error: $($error[0].Exception.Message)"
+    }
+}
